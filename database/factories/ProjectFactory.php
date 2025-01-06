4ec01c5c4b4ed93f745b82b6814ce93fd843e440
mkaminsk95 <m.kaminsk95@gmail.com>
2025-01-06 18:32:19 +0100

    Create factory with seeder

---
+++ database/factories/ProjectFactory.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProjectFactory extends Factory
+{
+    public function definition(): array
+    {
+        $startDate = $this->faker->date();
+        $endDate = $this->faker->dateTimeBetween($startDate, '+2 months');
+
+        return [
+            'name' => $this->faker->sentence(),
+            'description' => $this->faker->paragraph(),
+            'start_date' => $startDate,
+            'end_date' => $endDate,
+        ];
+    }
+}
