8d1798f8c5d748bee4c1870b58e8e01671d35120
mkaminsk95 <m.kaminsk95@gmail.com>
2025-01-06 19:08:08 +0100

    Make description nullable

--- database/migrations/2025_01_05_173955_create_projects_table.php
+++ database/migrations/2025_01_05_173955_create_projects_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->timestamps();
             $table->string('name', 256);
-            $table->text('description');
+            $table->text('description')->nullable();
             $table->date('start_date');
             $table->date('end_date')->nullable();
         });
