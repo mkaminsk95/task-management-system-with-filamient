b6a756dd1423cfab35adacb22f02008b4d3b40b6
mkaminsk95 <m.kaminsk95@gmail.com>
2025-01-07 09:44:56 +0100

    Add Project filament resource

---
+++ app/Filament/Resources/ProjectResource.php
@@ -0,0 +1,94 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ProjectResource\Pages;
+use App\Filament\Resources\ProjectResource\RelationManagers;
+use App\Models\Project;
+use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\Textarea;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+
+class ProjectResource extends Resource
+{
+    protected static ?string $model = Project::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name')
+                    ->label('Name')
+                    ->required()
+                    ->minLength(3)
+                    ->maxLength(255)
+                    ->placeholder('Enter the project name'),
+                Textarea::make('description')
+                    ->label('Description')
+                    ->placeholder('Enter the project description'),
+                DatePicker::make('start_date')
+                    ->label('Start Date')
+                    ->required()
+                    ->placeholder('Enter the start date'),
+                DatePicker::make('end_date')
+                    ->label('End Date')
+                    ->after('start_date')
+                    ->placeholder('Enter the end date')
+                    ->reactive()
+                    ->nullable(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('description')
+                    ->limit(50),
+                TextColumn::make('start_date')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('end_date')
+                    ->searchable()
+                    ->sortable(),
+            ])
+            ->filters([
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListProjects::route('/'),
+            'create' => Pages\CreateProject::route('/create'),
+            'edit' => Pages\EditProject::route('/{record}/edit'),
+        ];
+    }
+}
