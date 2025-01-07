59b1d3c80cdca8eacbb5a7afea622a5f6aebb385
mkaminsk95 <m.kaminsk95@gmail.com>
2025-01-07 11:23:17 +0100

    Adjust code to PhpStan

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -2,11 +2,13 @@
 
 namespace App\Models;
 
+use Database\Factories\ProjectFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model
 {
+    /** @use HasFactory<ProjectFactory> */
     use HasFactory;
 
     protected $guarded = [
