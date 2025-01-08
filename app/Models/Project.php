3527d3019f8b2b3237e410c2ce9b3dcecaa73f95
mkaminsk95 <m.kaminsk95@gmail.com>
2025-01-08 09:25:18 +0100

    Modify Project model so it contains tasks

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Database\Factories\ProjectFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Project extends Model
 {
@@ -16,4 +17,9 @@ class Project extends Model
         'created_at',
         'updated_at',
     ];
+
+    public function tasks(): HasMany
+    {
+        return $this->hasMany(Task::class);
+    }
 }
