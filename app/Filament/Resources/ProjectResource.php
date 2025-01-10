c87de91c823303422a6a6dd40da93fb07304f41c
mkaminsk95 <m.kaminsk95@gmail.com>
2025-01-10 11:32:36 +0100

    Add backend validation

--- app/Filament/Resources/ProjectResource.php
+++ app/Filament/Resources/ProjectResource.php
@@ -31,20 +31,24 @@ class ProjectResource extends Resource
                     ->required()
                     ->minLength(3)
                     ->maxLength(255)
-                    ->placeholder('Enter the project name'),
+                    ->placeholder('Enter the project name')
+                    ->rules('required|string|min:3'),
                 Textarea::make('description')
                     ->label('Description')
                     ->placeholder('Enter the project description'),
                 DatePicker::make('start_date')
                     ->label('Start Date')
                     ->required()
-                    ->placeholder('Enter the start date'),
+                    ->placeholder('Enter the start date')
+                    ->default(now())
+                    ->rules('required|date'),
                 DatePicker::make('end_date')
                     ->label('End Date')
                     ->after('start_date')
                     ->placeholder('Enter the end date')
                     ->reactive()
-                    ->nullable(),
+                    ->nullable()
+                    ->rules('sometimes|date|after:start_date'),
             ]);
     }
 
